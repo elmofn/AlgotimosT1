b1d1ae98229e292bb6442c9ca0bd01c392c01bbc
marlon <marlon673@hotmail.com>
2018-08-27 23:34:17 -0300

    arquivo main
    adicionado arquivo main com as variáveis e primeiras instruções.

---
+++ PrimeiroT1MarlonMartinsElmoNunes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+/* FACULDADE DOM BOSCO DE PORTO ALEGRE
+* Sistemas de Informação - Algoritmos
+*
+* Programa PrimeiroT1:
+* -------------------
+* Objetivo do Programa: Calcular a quantidade de quilowatts e o valor a ser pago por uma residencia. 
+* Componentes do grupo: Elmo Nunes e Marlon Martins
+* Data de entrega do arquivo fonte: ????
+*/
+
+  double salarioMin = 954.00,quilowattValor,quilowattQtd;
+
+  quilowattValor = salarioMin / 6;
+
+  printf("Digite a quantidade de quilowatts consumidos: ");
+  scanf("%s\n", &quilowattQtd);
+
+}
